db0cf441153b0e45392df6f60011e7bde1308f86
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-07-30 22:25:50 +0900

    ECサイト中級 #中級ログイン認証④　実行にてエラー有→UserCreateComplete.java修正（テキスト内スペルミスあり）

--- src/com/diworksdev/ecsite/action/UserCreateCompleteAction.java
+++ src/com/diworksdev/ecsite/action/UserCreateCompleteAction.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.diworksdev.ecsite.dao.UserCreateCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserCreateCompleteAction extends ActionSupport implements SessionAware{
@@ -12,10 +13,11 @@ public class UserCreateCompleteAction extends ActionSupport implements SessionAw
 	 private String loginPassword;
 	 private String userName;
 	 public Map<String,Object> session;
+
 	 private UserCreateCompleteDAO userCreateCompleteDAO = new UserCreateCompleteDAO();
 
 	 public String execute() throws SQLException{
-		 userCreateCompleteDAO.cerateUser(session.get("loginUserId").toString(),
+		 userCreateCompleteDAO.createUser(session.get("loginUserId").toString(),
 				 session.get("loginPassword").toString(),
 				 session.get("userName").toString());
 		 String result = SUCCESS;
