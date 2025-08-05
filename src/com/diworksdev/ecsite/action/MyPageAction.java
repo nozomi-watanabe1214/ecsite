4e531ee7c1b760d87b21e4ddd5d26d9dc179010c
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-08-05 22:38:19 +0900

    ECサイト中級 #中級ログイン認証⑤　完了ボタン押下できず→buyItemConfirm.jspエラー(jquery適用の記載漏れ)

---
+++ src/com/diworksdev/ecsite/action/MyPageAction.java
@@ -0,0 +1,13 @@
+package com.diworksdev.ecsite.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+import com.diworksdev.ecsite.dao.MyPageDAO;
+import com.diworksdev.ecsite.dto.MyPageDTO;
+
+public class MyPageAction extends ActionSupport imlements SessionAware{
+
+}
