16261ffda457da9f4ea2bef31c80f364298c7e4b
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-08-07 22:16:36 +0900

    ECサイト中級 #中級ログイン認証⑥　MyPageDAO.java追記（1文抜けあり）

--- src/com/diworksdev/ecsite/dao/MyPageDAO.java
+++ src/com/diworksdev/ecsite/dao/MyPageDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.diworksdev.ecsite.dto.MyPageDTO;
 import com.diworksdev.ecsite.util.DBConnector;
 
 public class MyPageDAO {
@@ -14,6 +15,7 @@ public class MyPageDAO {
 	dbConnector.getConnection();
 
 	public ArrayList<MyPageDTO> getMyPageUserInfo(String item_transaction_id, String user_master_id) throws SQLException{
+		ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
 		String sql = "SELECT ubit.id, iit.item_name, ubit.total_price, ubit.total_count, ubit.pay, ubit.insert_date "
 				+ "FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id "
 				+ "WHERE ubit.item_transaction_id = ? AND ubit.user_master_id = ? ORDER BY isert_date DESC";
